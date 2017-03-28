76d45ed365b0f740f0e15f2db486db2398e713f9
247Kittichai <tae.a3.tt@gmail.com>
2017-03-28 09:30:22 +0700

    Add files via upload

---
+++ LAB1/S-O-S/S-O-S.ino
@@ -0,0 +1,64 @@
+// สัญญาณขอความช่วยเหลือ SOS คำว่า (HELP)ช่วย
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13,HIGH);//H
+delay(300);
+digitalWrite(13,LOW);//H
+delay(300);
+digitalWrite(13,HIGH);//H
+delay(300);
+digitalWrite(13,LOW);//H
+delay(300);
+digitalWrite(13,HIGH);//H
+delay(300);
+digitalWrite(13,LOW);//H
+delay(300);
+digitalWrite(13,HIGH);//H
+delay(300);
+digitalWrite(13,LOW);//H
+delay(300);
+
+digitalWrite(13,HIGH);//E
+delay(300);
+digitalWrite(13,LOW);//E
+delay(300);
+
+digitalWrite(13,HIGH);//L
+delay(300);
+digitalWrite(13,LOW);//L
+delay(300);
+digitalWrite(13,HIGH);//L
+delay(1000);
+digitalWrite(13,LOW);//L
+delay(1000);
+digitalWrite(13,HIGH);//L
+delay(300);
+digitalWrite(13,LOW);//L
+delay(300);
+digitalWrite(13,HIGH);//L
+delay(300);
+digitalWrite(13,LOW);//L
+delay(300);
+
+digitalWrite(13,HIGH);//P
+delay(300);
+digitalWrite(13,LOW);//P
+delay(300);
+digitalWrite(13,HIGH);//P
+delay(1000);
+digitalWrite(13,LOW);//P
+delay(1000);
+digitalWrite(13,HIGH);//P
+delay(1000);
+digitalWrite(13,LOW);//P
+delay(1000);
+digitalWrite(13,HIGH);//P
+delay(300);
+digitalWrite(13,LOW);//P
+delay(300);
+}
